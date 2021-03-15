44d4428cf5cee53d915cbea8f6b6184ba47c8503
Suhas Mohamedali <suhaz04@gmail.com>
2021-03-15 22:17:01 +1030

    Project initialed . Committed to local Git

---
+++ dew-weather-app/src/main/java/com/dew/weatherapp/DewWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.dew.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DewWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DewWeatherAppApplication.class, args);
+	}
+
+}
---
+++ dew-weather-app/src/test/java/com/dew/weatherapp/DewWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dew.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DewWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
